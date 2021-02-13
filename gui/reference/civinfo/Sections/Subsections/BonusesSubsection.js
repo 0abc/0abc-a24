a73e12779795cc43ba2a0eba50a65ded5295eee1
0abc <0abc@mail.com>
2021-02-13 20:13:49 +0100

    delete team bonuses

---
+++ gui/reference/civinfo/Sections/Subsections/BonusesSubsection.js
@@ -0,0 +1,36 @@
+class BonusesSubsection extends Subsection
+{
+	constructor(page)
+	{
+		super(page);
+		this.CivBonuses = Engine.GetGUIObjectByName("civBonuses");
+	}
+
+	update(civCode, civInfo)
+	{
+		// Not all civ bonuses can be represented by a single auto-researched technology (e.g.
+		// Athenian "Silver Owls", Roman "Testudo Formation"). Thus we also display descriptions
+		// of civ bonuses as written in the {civ}.json files.
+		let civBonuses = this.getTechnologyCaptions(
+			this.page.TemplateLoader.autoResearchTechList,
+			civCode
+		);
+		for (let bonus of civInfo.CivBonuses)
+			civBonuses.push(this.page.formatEntry(
+				bonus.Name,
+				bonus.History || false,
+				bonus.Description || false
+			));
+		civBonuses.unshift(
+			this.page.formatHeading(
+				this.HeadingCivBonusCaption(civBonuses.length),
+				this.page.SubsectionHeaderSize
+			)
+		);
+
+		this.CivBonuses.caption = civBonuses.join("\n");
+	}
+}
+
+BonusesSubsection.prototype.HeadingCivBonusCaption =
+	count => translatePlural("Civilization Bonus", "Civilization Bonuses", count);
